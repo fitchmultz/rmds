b2ddee9be6fb1c0cbc0131bb582f973c3c36010e
Mitch Fultz <mitchfultz@gmail.com>
2025-05-06 10:55:57 -0600

    Update formula with actual SHA256 hash

--- rmds.rb
+++ rmds.rb
@@ -2,7 +2,7 @@ class Rmds < Formula
   desc "Remove all .DS_Store files recursively from current directory"
   homepage "https://github.com/fitchmultz/rmds"
   url "https://github.com/fitchmultz/rmds/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  sha256 "0e7fb20e950282f7574ce5c86ac714d373b9e2c629ad405aacf093e60a9f556d"
   license "MIT"
 
   def install
